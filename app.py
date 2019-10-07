8d955bf7c10ffe1f937afb3c60ab1400c4b92592
Arpitgoyalgg <50814175+Arpitgoyalgg@users.noreply.github.com>
2019-10-07 17:12:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,7 @@ given_str = input("Enter a string: ")
 set_alphabets_common = {'a': 0,'r': 0,'p': 0,'i': 0,'t': 0}
 
 for key in given_str:
-    if key == 'a':
+    if key == 'a' or key == 'r' or key == 'p' or key == 'i' or key == 't':
         set_alphabets_common[key] = set_alphabets_common[key]+1
-    if key == 'r':
-        set_alphabets_common[key] = set_alphabets_common[key]+1
-    if key == 'p':
-        set_alphabets_common[key] = set_alphabets_common[key]+1
-    if key == 'i':
-        set_alphabets_common[key] = set_alphabets_common[key]+1
-    if key == 't':
-        set_alphabets_common[key] = set_alphabets_common[key]+1
-
-
+    
 print(set_alphabets_common)
